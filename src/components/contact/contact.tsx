99e3aafacae0982f34d95dd7d964dec0527933c3
jralvarezwindey <jralvarezwindey@gmail.com>
2023-07-04 15:35:43 -0300

    Contact section styles improved.

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -2,20 +2,40 @@ import "./contact.scss";
 
 export default function Contact() {
   return (
-    <div className="contact d-flex flex-column">
+    <div className="contact">
       <h1 className="section-title">Want to know more? Reach out!</h1>
 
-      <label htmlFor="name">Name</label>
-      <input type="text" name="name" />
+      <div className="form-container row">
+        <div className="col-6 p-4 ps-0">
+          <label htmlFor="first-name">First name:</label>
+          <input type="text" name="first-name" />
+        </div>
 
-      <label htmlFor="email">Email</label>
-      <input type="text" name="email" />
+        <div className="col-6 p-4 pe-0">
+          <label htmlFor="last-name">Last name:</label>
+          <input type="text" name="last-name" />
+        </div>
 
-      <label htmlFor="subject">Subject</label>
-      <input type="text" name="subject" />
+        <div className="col-6 p-4 ps-0">
+          <label htmlFor="email">Email:</label>
+          <input type="text" name="email" />
+        </div>
 
-      <label htmlFor="message">Message</label>
-      <input type="text" name="message" />
+        <div className="col-6 p-4 pe-0">
+          <label htmlFor="subject">Subject:</label>
+          <input type="text" name="subject" />
+        </div>
+
+        <div className="col-12 py-4 px-0">
+          <label htmlFor="message">Message:</label>
+          <textarea
+            className="h-auto"
+            name="message"
+            cols={30}
+            rows={10}
+          ></textarea>
+        </div>
+      </div>
     </div>
   );
 }
