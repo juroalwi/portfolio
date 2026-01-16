e9f3af91cee4992afcae3a579102d79249a3bed9
Julian Alvarez <julian.alvarez@bitnovo.com>
2026-01-16 12:17:52 -0300

    make bg dots span further away from screen border

--- src/components/p5-sketch/dynamic-graphs-background.tsx
+++ src/components/p5-sketch/dynamic-graphs-background.tsx
@@ -11,6 +11,7 @@ type Dot = {
 
 const sketch = (p: p5) => {
   const DOTS_LIMIT = 40;
+  const INTERACTION_DISTANCE = 200;
   let dots: Dot[] = [];
 
   p.setup = () => {
@@ -35,6 +36,10 @@ const sketch = (p: p5) => {
 
   p.draw = () => {
     p.background(255);
+    const topBorder = -INTERACTION_DISTANCE / 2;
+    const rightBorder = p.width + INTERACTION_DISTANCE / 2;
+    const bottomBorder = p.height + INTERACTION_DISTANCE / 2;
+    const leftBorder = -INTERACTION_DISTANCE / 2;
 
     if (dots.length < DOTS_LIMIT) {
       const side = p.floor(p.random(4)); // 0: top, 1: right, 2: bottom, 3: left
@@ -42,21 +47,21 @@ const sketch = (p: p5) => {
 
       if (side === 0) {
         x = p.random(p.width);
-        y = 0;
+        y = topBorder;
         vx = p.random(-1, 1);
         vy = p.random(0.5, 2);
       } else if (side === 1) {
-        x = p.width;
+        x = rightBorder;
         y = p.random(p.height);
         vx = p.random(-2, -0.5);
         vy = p.random(-1, 1);
       } else if (side === 2) {
         x = p.random(p.width);
-        y = p.height;
+        y = bottomBorder;
         vx = p.random(-1, 1);
         vy = p.random(-2, -0.5);
       } else {
-        x = 0;
+        x = leftBorder;
         y = p.random(p.height);
         vx = p.random(0.5, 2);
         vy = p.random(-1, 1);
@@ -83,7 +88,7 @@ const sketch = (p: p5) => {
         if (dot === otherDot) continue;
 
         const distance = p.dist(dot.x, dot.y, otherDot.x, otherDot.y);
-        const quotient = distance / 200;
+        const quotient = distance / INTERACTION_DISTANCE;
         if (quotient <= 1) {
           p.stroke(180, 180, 180, 200 * (1 - quotient) ** 2);
           p.line(dot.x, dot.y, otherDot.x, otherDot.y);
@@ -93,10 +98,10 @@ const sketch = (p: p5) => {
 
     dots = dots.filter((dot) => {
       const shouldRemove =
-        dot.x < -dot.size ||
-        dot.x > p.width + dot.size ||
-        dot.y < -dot.size ||
-        dot.y > p.height + dot.size;
+        dot.x < -dot.size + leftBorder ||
+        dot.x > dot.size + rightBorder ||
+        dot.y < -dot.size + topBorder ||
+        dot.y > dot.size + bottomBorder;
 
       return !shouldRemove;
     });
