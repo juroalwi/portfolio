76382e3aeba35ae4611e973b865d85288614a082
Julian Alvarez <juroalwi@gmail.com>
2026-01-16 12:18:08 -0300

    fade in about descr

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import TypedTitle from "./typed-title/typed-title";
 import poi from "assets/poi.png";
+import { FadeIn } from "./fade-in";
 
 export default function About() {
   const [mainTitleTyped, setMainTitleTyped] = useState(false);
 
   return (
-    <div className="flex items-center justify-between gap-20">
+    <div className="flex items-center justify-between gap-20 w-[55%] max-w-[1000px]">
       <div>
         <TypedTitle
           className="mb-1"
@@ -24,19 +25,21 @@ export default function About() {
           Full stack developer & Mathematician
         </TypedTitle>
 
-        <p className="text-lg">
-          More than 3 years of experience building scalable and efficient
-          systems using Typescript, NestJS, PostgreSQL and React.
-          <br />I have a genuine enthusiasm for continuous learning and I thrive
-          in collaborative environments. Known for my meticulous attention to
-          detail, I consistently strive to deliver work of the highest possible
-          standard.
-        </p>
+        <FadeIn delay={150} duration={800}>
+          <p className="text-lg">
+            More than 3 years of experience building scalable and efficient
+            systems using Typescript, NestJS, PostgreSQL and React.
+            <br />I have a genuine enthusiasm for continuous learning and I
+            thrive in collaborative environments. Known for my meticulous
+            attention to detail, I consistently strive to deliver work of the
+            highest possible standard.
+          </p>
+        </FadeIn>
       </div>
 
-      <div className="relative rounded-full bg-lime-100/80 flex w-84 h-84 shrink-0 shadow-lg">
+      <div className="relative rounded-full bg-lime-100/60 flex w-84 h-84 shrink-0 shadow-lg">
         <img
-          className="absolute bottom-3 right-3 rounded-full bg-blue-100 w-84 h-84 object-contain aspect-square shadow-lg"
+          className="absolute bottom-3 right-3 rounded-full bg-blue-50 w-84 h-84 object-contain aspect-square shadow-lg"
           src={poi}
         />
       </div>
