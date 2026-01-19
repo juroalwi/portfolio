bded85664cc9c046bd2af1b05b0536fdcca3f232
Julian Alvarez <juroalwi@gmail.com>
2026-01-19 11:06:22 -0300

    use real projects

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -1,9 +1,10 @@
 import { twMerge } from "tailwind-merge";
 
 type ProjectCardProps = {
-  id: number;
+  id: string;
   img: string;
   name: string;
+  url: string;
   description: string;
   textClassName?: string;
   big?: boolean;
@@ -13,19 +14,23 @@ export default function ProjectCard({
   id,
   name,
   img,
+  url,
   description,
   textClassName,
   big,
 }: ProjectCardProps) {
   return (
     <div
-      className="h-full w-full relative group rounded-xl shadow-lg overflow-hidden cursor-pointer"
+      className="h-full w-full relative group rounded-xl shadow-lg overflow-hidden cursor-pointer bg-secondary/3"
       key={id}
+      onClick={() => {
+        window.open(url, "_blank");
+      }}
     >
       <div className="absolute inset-0">
         <img
           src={img}
-          className="w-full h-full object-cover transition-transform duration-200 group-hover:scale-105"
+          className="w-full h-full object-contain transition-transform duration-300 group-hover:scale-105"
           alt={name}
         />
         <div className="absolute inset-0 bg-linear-to-t from-dark/80 via-dark/40 to-transparent" />
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,43 +1,46 @@
 import { FadeIn } from "./fade-in";
 import ProjectCard from "./project-card";
 import TypedTitle from "./typed-title/typed-title";
-import img from "assets/chinardo.png";
+import weatherAppImg from "assets/weather-app.svg";
+import cookEmAllImg from "assets/cook-em-all.svg";
+import schoolsLicitationImg from "assets/schools-licitationn.png";
+import femImg from "assets/fem.png";
 
 type ProjectData = {
-  id: number;
   name: string;
   img: any;
   description: string;
+  url: string;
 };
 
 const projects: ProjectData[] = [
   {
-    id: 1,
-    name: "Project 1",
-    img: img,
+    name: "Cook 'Em All",
+    img: cookEmAllImg,
     description:
-      "Lorem ipsum ap dor un ideal a izquierda es maximal si no existe ideal propio que lo contenga estrictamente.Lorem ipsum ap dor un ideal a izquierda es maximal si no existe ideal propio que lo contenga estrictamente",
+      "A responsive single-page recipe platform that lets users discover recipes by name, filter and sort results, create custom recipes, and explore detailed recipe views—all in a smooth, fast UI.",
+    url: "https://jralvarezwindey-food-app.vercel.app/landing",
   },
   {
-    id: 2,
-    name: "Project 2",
-    img: img,
+    name: "Finite Elements Method",
+    img: femImg,
     description:
-      "Lorem ipsum ap dor un ideal a izquierda es maximal si no existe ideal propio que lo contenga estrictamente",
+      "From-scratch FEM implementation in Julia to solve a reaction–diffusion PDE on a unit square using triangular elements.",
+    url: "https://github.com/juroalwi/finite-elements-method",
   },
   {
-    id: 3,
-    name: "Project 3",
-    img: img,
+    name: "Schools auction",
+    img: schoolsLicitationImg,
     description:
-      "Lorem ipsum ap dor un ideal a izquierda es maximal si no existe ideal propio que lo contenga estrictamente",
+      "Linear programming sandbox reproducing results from a real procurement auction study, showing how dominant coverage and quantity discounts shape optimal allocations.",
+    url: "https://github.com/juroalwi/auction-linear-programming",
   },
   {
-    id: 4,
-    name: "Project 4",
-    img: img,
+    name: "Weather App",
+    img: weatherAppImg,
     description:
-      "Lorem ipsum ap dor un ideal a izquierda es maximal si no existe ideal propio que lo contenga estrictamente",
+      "A lightweight weather app powered by a third-party API, focused on fast lookup, clean UI, and real-time data display.",
+    url: "https://github.com/juroalwi/weather-app",
   },
 ];
 
@@ -49,10 +52,11 @@ export default function Projects() {
         <div className="row-span-6 col-span-5">
           <FadeIn delay={0} from="top" className="h-full w-full">
             <ProjectCard
-              id={projects[0].id}
+              id="project-0"
               name={projects[0].name}
               img={projects[0].img}
               description={projects[0].description}
+              url={projects[0].url}
               big
             />
           </FadeIn>
@@ -61,10 +65,11 @@ export default function Projects() {
         <div className="row-span-3 col-span-4">
           <FadeIn delay={50} from="top" className="h-full w-full">
             <ProjectCard
-              id={projects[1].id}
+              id="project-1"
               name={projects[1].name}
               img={projects[1].img}
               description={projects[1].description}
+              url={projects[1].url}
             />
           </FadeIn>
         </div>
@@ -72,10 +77,11 @@ export default function Projects() {
         <div className="row-span-3 col-span-3">
           <FadeIn delay={100} from="top" className="h-full w-full">
             <ProjectCard
-              id={projects[2].id}
+              id="project-2"
               name={projects[2].name}
               img={projects[2].img}
               description={projects[2].description}
+              url={projects[2].url}
             />
           </FadeIn>
         </div>
@@ -83,10 +89,11 @@ export default function Projects() {
         <div className="row-span-3 col-span-6">
           <FadeIn delay={150} from="right" className="h-full w-full">
             <ProjectCard
-              id={projects[3].id}
+              id="project-3"
               name={projects[3].name}
               img={projects[3].img}
               description={projects[3].description}
+              url={projects[3].url}
             />
           </FadeIn>
         </div>
