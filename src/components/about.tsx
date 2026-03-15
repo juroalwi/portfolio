e989a113cb7ab6dbac0d5057bedd8468326f878e
Julian Alvarez <juroalwi@gmail.com>
2026-03-15 15:04:09 -0300

    hero img better size computation

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,33 +1,20 @@
-import { useState } from "react";
 import { TypedTitle } from "components/typed-title/typed-title";
 import { FadeIn } from "components/fade-in";
 import poi from "assets/images/poi.webp";
 
 export const About = () => {
-  const [mainTitleTyped, setMainTitleTyped] = useState(false);
-
   return (
-    <div className="flex w-full max-w-300 flex-col-reverse items-center justify-between gap-0 lg:flex-row lg:gap-20">
+    <div className="flex w-full max-w-250 flex-col-reverse items-center justify-between gap-8 lg:flex-row lg:gap-20">
       <div>
         <TypedTitle
-          className="mb-0 text-center lg:mb-1 lg:text-left"
-          onFinishTyping={() => setMainTitleTyped(true)}
-          caretHidden={mainTitleTyped}
+          title="Hi there, this is Julian"
+          subtitle="Full stack engineer & Mathematician"
           typingSpeedMs={40}
-        >
-          Hi there, this is Julian
-        </TypedTitle>
-        <TypedTitle
-          className="mb-4 text-center lg:mb-6 lg:text-left"
-          typingSpeedMs={30}
-          small
-          disabled={!mainTitleTyped}
-        >
-          Full stack engineer & Mathematician
-        </TypedTitle>
+          className="mb-4 flex flex-col text-center lg:mb-4 lg:text-left"
+        />
 
         <FadeIn delay={200}>
-          <p className="w-full p-4 text-left text-base lg:p-0 lg:text-lg">
+          <p className="px-4 text-justify text-sm lg:px-0 lg:text-lg">
             Four years of experience building scalable and efficient systems
             using Typescript, NestJS, PostgreSQL and React.
             <br />I have a genuine enthusiasm for continuous learning and I
@@ -38,19 +25,19 @@ export const About = () => {
         </FadeIn>
       </div>
 
-      <FadeIn delay={100}>
-        <div className="relative size-0.5 h-84 w-84 shrink-0 scale-75 lg:scale-none">
+      <FadeIn delay={100} className="shrink-0" from="top">
+        <div className="relative h-60 w-60 lg:h-80 lg:w-80">
           <div
-            className="bg-primary/40 absolute inset-0 rounded-full shadow-lg"
+            className="bg-primary/40 absolute h-full w-full rounded-full"
             style={{
               maskImage:
-                "radial-gradient(circle 168px at calc(50% - 12px) calc(50% - 12px), transparent 168px, black 169px)",
+                "radial-gradient(circle farthest-side at 45% 45%, transparent 90.9%, black 90.9%)",
               WebkitMaskImage:
-                "radial-gradient(circle 168px at calc(50% - 12px) calc(50% - 12px), transparent 168px, black 169px)",
+                "radial-gradient(circle farthest-side at 45% 45%, transparent 90.9%, black 90.9%)",
             }}
           />
           <img
-            className="bg-secondary/40 absolute right-3 bottom-3 z-10 aspect-square h-84 w-84 rounded-full object-contain shadow-lg"
+            className="bg-secondary/40 absolute right-[5%] bottom-[5%] h-full w-full rounded-full object-contain shadow-lg"
             src={poi}
           />
         </div>
